9e84b4a7916d5ae48ce737d3877927dbae180584
Urameshi801 <70042912+Urameshi801@users.noreply.github.com>
2020-10-10 23:35:36 -0600

    Add files via upload

---
+++ Homework-4-/script.js
@@ -0,0 +1,12 @@
+// html that needs to be manipulated//
+var questionare = document.getElementById("quiz");
+var resultsElem = document.getElementById("results");
+var finalScoreElem = document.getElementById("finalScore");
+var gameoverElem = document.getElementById("gameOver");
+var questionElem = document.getElementById("questions");
+var quizTimer = document.getElementById("timer")
+var startQuizBttn = document.getElementById("StartButton");
+var startQuizElem = document.getElementById("startScreen")
+var highScoreContainer = document.getElementById("highScoreContianer");
+var highScoreElem = document.getElementById("highScoreScreen");
+var 
